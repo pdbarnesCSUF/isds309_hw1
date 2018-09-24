4890862abb8e61d7765faac84a66d129c139fdca
Patrick B <zz-pbarnes@fullerton.edu>
2018-09-24 14:20:17 -0700

    converted to visual studio projects
    completed hours and minutes

---
+++ InchesToCentimeterslnteractive/InchesToCentimeterslnteractive.cs
@@ -0,0 +1,49 @@
+﻿/*
+    Patrick Barnes
+    https://github.com/pdbarnesCSUF/isds309_hw1
+*/
+/*
+5
+-
+Write a C# program named InchesToCentmeters that declares a named constant that
+holds the number of centimeters in an inch: 2.54. Also declare a variable to
+represent a measurement in inches, and assign a value. Display the measurement
+in both inches and centimeters—for example, 3 inches is 7.62 centimeters.
+---
+6
+-
+Convert the InchesToCentimeters program to an interactive application named
+InchesToCentimeterslnteractive. Instead of assigning a value to the inches
+variable, accept the value from the user as input.
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using static System.Console;
+
+namespace InchesToCentmeters
+{
+    class InchesToCentimeterslnteractive
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("hw1#6 - Patrick Barnes");
+            WriteLine("InchesToCentimeters");
+
+            const double CM_PER_IN = 2.54;
+            double inputInches = 0;
+            double convertedCentimeters = 0;
+
+            //skip error checking
+            Write("Input inches:");
+
+            inputInches = double.Parse(ReadLine());
+            convertedCentimeters = inputInches * CM_PER_IN;
+
+            WriteLine("{0} inches is {1} centimeters.", inputInches, convertedCentimeters);
+
+            WriteLine("--End--");
+        }
+    }
+}
